decbdb3222e322d14459530ef7ce19e4504c3618
David Orihuela <damiocid@gmail.com>
2017-03-10 20:29:46 -0500

    Add PUT todos/:id

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/todos', (req, res) => {
 //GET single
 app.get('/todos/:id', (req, res) => {
     let todoId = parseInt(req.params.id, 10),
-        matchedTodo = _.findWhere(todos, {id: todoId})
+        matchedTodo = _.findWhere(todos, {id: todoId});
 
     if(matchedTodo){
         res.json(matchedTodo);
@@ -60,6 +60,36 @@ app.delete('/todos/:id', (req, res) => {
             todos = _.without(todos, matchedTodo);
             res.json(matchedTodo);    
         }
+});
+
+//UPDATE
+app.put('/todos/:id', (req, res) => {
+    let todoId = parseInt(req.params.id, 10),
+        matchedTodo = _.findWhere(todos, {id: todoId}),
+        body =  _.pick(req.body, 'desc', 'isDone'),
+        validAttributes = {};
+
+        if(!matchedTodo){
+        return res.status(404).send();
+        }
+
+        //isDone validation
+        if(body.hasOwnProperty('isDone') && _.isBoolean(body.isDone)){
+            validAttributes.isDone = body.isDone;
+        } else if ( body.hasOwnProperty('isDone')){
+            return res.status(400).send();
+        }
+
+        //desc validation
+        if(body.hasOwnProperty('desc') && _.isBoolean(body.desc) && body.desc.trim().length > 0){
+            validAttributes.desc = body.desc;
+        } else if ( body.hasOwnProperty('desc')){
+            return res.status(400).send();
+        } 
+
+        //Objects in js are passed by reference!
+        _.extend(matchedTodo, validAttributes);
+        res.json(matchedTodo);
 });
 
 app.listen(port, () => {
